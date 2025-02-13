b373b9ea9902c03a78d008629570278a56967ea2
supersakana <zacwilliamson@icloud.com>
2025-02-12 19:05:04 -0600

    added makefile

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
